b46e6db7691b4bed904fc6cb6db92aa5a3cc2f37
Matthew Zhu <matt.z496@gmail.com>
2022-02-12 00:44:09 -0800

    Remove reading time

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const pluginReadingTime = require('eleventy-plugin-reading-time');
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
@@ -11,7 +10,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(pluginReadingTime);
 
   eleventyConfig.setDataDeepMerge(true);
 
